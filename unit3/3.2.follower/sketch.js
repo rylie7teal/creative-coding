50d98f8edce79f821f42fb49665dd8a7de6a0063
rylie7teal <rvann@mail.umw.edu>
2024-10-28 23:04:01 -0400

    unit 3

--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -1,7 +1,56 @@
+let debug = false;
+let speedfactor = 4;
+let xspeed = speedfactor;
+let yspeed = speedfactor;
+let x = 0;
+let y = 0;
+let d = 0;
+
 function setup() {
   createCanvas(400, 400);
+
+  x = random(width);
+  y = random(height);
 }
 
 function draw() {
   background(220);
+
+  noStroke();
+  
+  d = sqrt((x-mouseX)**2 + (y-mouseY)**2);
+
+  x += xspeed;
+  y += yspeed;
+  
+  circle(x, y, 50);
+
+  if (mouseX > x){
+    xspeed = speedfactor;
+  }
+  else {
+    xspeed = -speedfactor;
+  }
+
+  if (mouseY > y){
+    yspeed = speedfactor;
+  }
+  else {
+    yspeed = -speedfactor
+  }
+
+  if (d < 25){
+    fill(random(0, 255), random(0, 255), random(0,255));
+    x = random(width);
+    y = random(height);
+  }
+
+  if (debug){
+    textSize (30);
+    text("mouseX:" + mouseX, 50, 50);
+    text ("mouseY" +mouseY, 50, 50);
+    text("x:" + x, 50, 120);
+    text ("y: " + y, 50, 150);
+    text ("d: " + d, 50, 180)
+  }
 }
